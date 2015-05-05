73f2b2fce10221e1732a04c1c185b8c0eb14ba78
missit <missit@localhost.localdomain>
2015-05-05 23:20:38 +0800

    配置简单的cmake构建脚本

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstddef>
+
+struct LinkNode
+{
+    int val;
+    LinkNode* next;
+
+    LinkNode(int value)
+        : val(value)
+          , next(nullptr)
+    {
+    }
+};
+
+int main()
+{
+}
+
