353b04717fa5f47add1fa2d758103eb83c0fe0e1
zhangjianjun <zhangfanwan@gmail.com>
2015-05-07 23:29:36 +0800

    筛选法来实现小于n的所有素数

--- count_primes.cpp
+++ count_primes.cpp
@@ -3,90 +3,66 @@
 #include<cmath>
 #include<bitset>
 #include<functional>
+#include<cstring>
+
+const static int8_t shift = 3;
+const static int8_t mask = 7;
+
+inline void bit_reset(int8_t* m, size_t l)
+{
+    m[l>>shift] &= ~(1 << (l&mask));
+}
+
+inline bool bit_test(int8_t* m, size_t l)
+{
+    return m[l>>shift] & (1 << (l&mask));
+}
+
+inline void bit_set(int8_t* m, size_t l)
+{
+    m[l>>shift] |= (1 << (l&mask));
+}
 
 class Solution
 {
 public:
-    int fast_power(int a, int b)
+    int count_primes(int n)
     {
-        int d = 1;
-        std::bitset<sizeof(b)*8> binary(b);
-        int k = get_msb(b);
+        int byte_len =(n>>3)+1;
+        int8_t* table = new int8_t[byte_len];
+        memset(table, '\0', byte_len);
 
-        for(int i=k; i>=0; --i)
+        auto bit_set = [](int8_t* t, size_t l)->void
         {
-            d = d * d;
-            if(binary.test(k))
+            t[l>>3] |= (1 << (l&7));
+        };
+
+        auto bit_test = [](int8_t* t, size_t l)->bool
+        {
+            return t[l>>3] & (1 << (l&7));
+        };
+
+        for(int i=2; i<n; ++i)
+        {
+            if(!bit_test(table, i))
             {
-                d = d * a;
+                for(int j=i + i; j<n; j+=i)
+                {
+                    bit_set(table, j);
+                }
             }
         }
-        return d;
-    }
 
-    int count_primes(int n, std::function<bool(int)> predicate)
-    {
-        assert(n>=2);
-        int count_of_primes = 1;
-        for(int i=3; i<n; i+=2)
+        int count_of_primes = 0;
+        for(int i=2; i<n; ++i)
         {
-            if(predicate(i))
+            if(!bit_test(table, i))
             {
                 ++count_of_primes;
             }
         }
+        delete[] table;
         return count_of_primes;
-    }
-
-    bool be_prime(int n)
-    {
-        if(modular_exponentiation(2, n-1, n) != 1)
-        {
-            return false;
-        }
-        return be_prime_base(n);
-    }
-
-    bool be_prime_base(int n)
-    {
-        int s_root = static_cast<int>(sqrt(n));
-        int i = (n % 2 == 0) ? 2 : 3;
-        for(; i<=s_root; i+=2)
-        {
-            if((n%i) == 0)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    int modular_exponentiation(int a, int b, int n)
-    {
-        int d = 1;
-        std::bitset<sizeof(b)*8> binary(b);
-        int k = get_msb(b);
-
-        for(int i=k; i>=0; --i)
-        {
-            d = (d * d) % n;
-            if(binary.test(i))
-            {
-                d = (d * a) % n;
-            }
-        }
-        return d;
-    }
-
-    int get_msb(int val)
-    {
-        std::bitset<sizeof(int)*8> binary(val);
-        for(int i=sizeof(int)*8-1; i>=0; --i)
-        {
-            if(binary.test(i))
-                return i;
-        }
-        return -1; //all zero
     }
 };
 
@@ -94,11 +70,6 @@ int main()
 {
     Solution s;
     int n = 1500000;
-    std::cout<<"count: "<<s.count_primes(n
-            , std::bind(&Solution::be_prime
-                , &s, std::placeholders::_1))<<std::endl;
-
-    for(int i=2; i<1500000; i++)
-        std::cout<<s.be_prime(i)<<" "<<s.be_prime_base(i)<<std::endl;
+    std::cout<<s.count_primes(n)<<std::endl;
 }
 
---
+++ tmp.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<cassert>
+#include<cmath>
+#include<bitset>
+#include<functional>
+
+class Solution
+{
+public:
+    int fast_power(int a, int b)
+    {
+        int d = 1;
+        std::bitset<sizeof(b)*8> binary(b);
+        int k = get_msb(b);
+
+        for(int i=k; i>=0; --i)
+        {
+            d = d * d;
+            if(binary.test(k))
+            {
+                d = d * a;
+            }
+        }
+        return d;
+    }
+
+    int count_primes(int n, std::function<bool(int)> predicate)
+    {
+        assert(n>=2);
+        int count_of_primes = 1;
+        for(int i=3; i<n; i+=2)
+        {
+            if(predicate(i))
+            {
+                ++count_of_primes;
+            }
+        }
+        return count_of_primes;
+    }
+
+    bool be_prime(int n)
+    {
+        if(modular_exponentiation(2, n-1, n) != 1)
+        {
+            return false;
+        }
+        return be_prime_base(n);
+    }
+
+    bool be_prime_base(int n)
+    {
+        int s_root = static_cast<int>(sqrt(n));
+        int i = (n % 2 == 0) ? 2 : 3;
+        for(; i<=s_root; i+=2)
+        {
+            if((n%i) == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int modular_exponentiation(int a, int b, int n)
+    {
+        int d = 1;
+        std::bitset<sizeof(b)*8> binary(b);
+        int k = get_msb(b);
+
+        for(int i=k; i>=0; --i)
+        {
+            d = (d * d) % n;
+            if(binary.test(i))
+            {
+                d = (d * a) % n;
+            }
+        }
+        return d;
+    }
+
+    int get_msb(int val)
+    {
+        std::bitset<sizeof(int)*8> binary(val);
+        for(int i=sizeof(int)*8-1; i>=0; --i)
+        {
+            if(binary.test(i))
+                return i;
+        }
+        return -1; //all zero
+    }
+};
+
