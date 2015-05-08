cb9d441067a7a5215151f840a347101feec3861e
zhangjianjun <zhangfanwan@gmail.com>
2015-05-08 14:55:41 +0800

    trie编译通过

--- trie_prefix_tree.cpp
+++ trie_prefix_tree.cpp
@@ -1,7 +1,11 @@
+#include<vector>
 #include<iostream>
 
 struct TrieNode
 {
+    std::vector<TrieNode*> children_;
+    char c_;
+    bool is_end_;
 };
 
 class Trie
@@ -13,15 +17,41 @@ public:
 
     void insert(const std::string& s)
     {
-    }
+        TrieNode* last_node = nullptr;
+        auto last_itr = s.cbegin();
 
-    bool search(const std::string& s)
-    {
-        return false;
+        if(find_last_char(&last_itr, s.cend(), root_, &last_node))
+        {
+            last_node->is_end_ = true;
+        }
+
+        TrieNode* root = last_node ? last_node : root_;
+        ++last_itr;
+        for(; last_itr!=s.cend(); ++last_itr)
+        {
+            root->children_.push_back(new TrieNode());
+            root = root->children_.back();
+        }
+        root->children_.back()->is_end_ = true;
     }
 
     bool startWith(const std::string& s)
     {
+        for(auto n : root_->children_)
+        {
+            if(search_internal(s.cbegin(), s.cend(), n))
+                return true;
+        }
+        return false;
+    }
+
+    bool search(const std::string& s)
+    {
+        for(auto n : root_->children_)
+        {
+            if(search_internal(s.cbegin(), s.cend(), n))
+                return true;
+        }
         return false;
     }
 
@@ -30,6 +60,62 @@ public:
         delete root_;
     }
 private:
+    using itr_type = std::string::const_iterator;
+
+    bool start_with_internal(itr_type first, itr_type last, TrieNode* root)
+    {
+        if(first==last)
+            return true;
+
+        if(root->c_!=*first)
+            return false;
+
+        for(auto n : root->children_)
+        {
+            if(start_with_internal(++first, last, n))
+                return true;
+        }
+        return false;
+    }
+
+    bool search_internal(itr_type first, itr_type last, TrieNode* root)
+    {
+        if(root->c_!= *first)
+            return false;
+
+        if(++first==last)
+            return root->is_end_;
+
+        for(auto n : root->children_)
+        {
+            if(search_internal(++first, last, n))
+                return true;
+        }
+        return false;
+    }
+
+    bool find_last_char(itr_type* first
+            , itr_type last
+            , TrieNode* root
+            , TrieNode** last_char)
+    {
+        if(*first==last)
+            return true;
+
+        if(root->c_!= **first)
+            return false;
+
+        *last_char = root;
+
+        for(auto n : root->children_)
+        {
+            *first = (*first)++;
+            if(find_last_char(first, last, n, last_char))
+                return true;
+        }
+        return false;
+    }
+
     TrieNode* root_;
 };
 
