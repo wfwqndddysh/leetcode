3fdd672ea4fd7087dbee778ceaf5da14204e3956
zhangjianjun <zhangfanwan@gmail.com>
2015-05-10 15:01:49 +0800

    reverse bits, 利用分割合并思想可以做到log(sizeof(n))

--- reverse_bits.cpp
+++ reverse_bits.cpp
@@ -14,6 +14,16 @@ public:
         }
         return r_n;
     }
+
+    //divide and conquer
+    uint32_t reverse_bits_log_n(uint32_t n)
+    {
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
 };
 
 int main()
