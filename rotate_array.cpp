eb0b70459df2a7e9cd01f632043c53b36e05beb8
zhangjianjun <zhangfanwan@gmail.com>
2015-05-10 21:04:04 +0800

    遇到边界条件麻烦时，还是列出来再写代码吧，否则会折磨死人的

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -5,9 +5,11 @@
 class Solution
 {
 public:
+    /*
     void rotate(std::vector<int>& nums, int k)
     {
         auto sz = nums.size();
+        k %= sz;
         std::vector<int> tmp(sz);
 
         for(size_t i=0; i<sz; ++i)
@@ -16,12 +18,43 @@ public:
         }
         nums = tmp;
     }
+    */
+
+    void rotate(std::vector<int>& nums, int k)
+    {
+        auto sz = nums.size();
+        k %= sz;
+
+        if(sz==0 || k==0)
+            return;
+
+        size_t cur = 0;
+        size_t init = 0;
+        size_t tmp_prev = nums[sz-k];
+        bool start = false;
+
+        for(size_t i=0; i<sz; ++i)
+        {
+            if(cur==init && start)
+            {
+                ++init;
+                cur = init;
+                tmp_prev = nums[sz-k+cur];
+                start = false;
+            }
+            start = true;
+            int tmp = nums[cur];
+            nums[cur] = tmp_prev;
+            tmp_prev = tmp;
+            cur = (cur+k)%sz;
+        }
+    }
 };
 
 int main()
 {
     Solution s;
-    std::vector<int> v = {1, 2, 3};
+    std::vector<int> v{1,2,3,4,5,6};
     s.rotate(v, 2);
     for(auto n : v)
         std::cout<<n<<std::endl;
