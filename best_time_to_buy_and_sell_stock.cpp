c0d936d9ce72212b548e9d185fd6fc24c867d782
zhangjianjun <zhangfanwan@gmail.com>
2015-05-11 10:48:18 +0800

    start fisrt hard

---
+++ best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cassert>
+#include<vector>
+
+class Solution
+{
+public:
+    int maxProfit(int k, std::vector<int>& prices)
+    {
+        int money = 0;
+        return money;
+    }
+};
+
+int main()
+{
+    return 0;
+}
+
