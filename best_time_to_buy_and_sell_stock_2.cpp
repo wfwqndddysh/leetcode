1a13390836d60e09083d55aaef8831387718769e
zhangjianjun <zhangfanwan@gmail.com>
2015-05-11 22:38:32 +0800

    一次性通过，对于边界条件复杂的情况，纸上画画，可以节省超过60%的时间

--- best_time_to_buy_and_sell_stock_2.cpp
+++ best_time_to_buy_and_sell_stock_2.cpp
@@ -10,14 +10,74 @@ public:
         if(prices.size()<2)
             return 0;
 
-        int money = 0;
+        int max_profit = 0;
+        int sell = -1;
+        int low = 0;
 
-        return money;
+        for(int i=0; i<(int)prices.size(); ++i)
+        {
+            if(prices[i]>prices[low])
+            {
+                max_profit += prices[i]-prices[low];
+                sell = low = i;
+            }
+            else if(prices[i]<prices[low])
+            {
+                low = i;
+            }
+        }
+        return max_profit;
     }
 };
 
 int main()
 {
+    Solution s;
+    std::vector<int> stock {1, 2, 4};
+    std::cout<<s.maxProfit(stock)<<std::endl;
     return 0;
 }
+
+
+/*
+if(max_profit>0)
+{
+    if((sell+1)==i)
+    {
+        if(prices[i]>prices[sell])
+        {
+            max_profit += prices[i]-prices[sell];
+            sell = i;
+        }
+        else if(prices[i]<prices[sell])
+        {
+            low = i;
+        }
+    }
+    else
+    {
+        if(prices[i]>prices[low])
+        {
+            max_profit += prices[i]-prices[low];
+            sell = low = i;
+        }
+        else if(prices[i]<prices[low])
+        {
+            low = i;
+        }
+    }
+}
+else
+{
+    if(prices[i]>prices[low])
+    {
+        max_profit = prices[i]-prices[low];
+        sell = low = i;
+    }
+    else if(prices[i]<prices[low])
+    {
+        low = i;
+    }
+}
+*/
 
