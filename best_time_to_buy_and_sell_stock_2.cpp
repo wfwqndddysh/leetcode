3c09034e32e6d3526da0eb78e1cebb7a5614d70a
zhangjianjun <zhangfanwan@gmail.com>
2015-05-11 17:20:55 +0800

    开始系列第二，这次需要使用动态规划了

---
+++ best_time_to_buy_and_sell_stock_2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cassert>
+#include<vector> 
+
+class Solution
+{
+public:
+    int maxProfit(std::vector<int>& prices)
+    {
+        if(prices.size()<2)
+            return 0;
+
+        int money = 0;
+
+        return money;
+    }
+};
+
+int main()
+{
+    return 0;
+}
+
