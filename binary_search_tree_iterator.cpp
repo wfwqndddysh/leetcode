46adcd6ddb702f3f1af9cb7a09a4fdc0445d44f9
zhangjianjun <zhangfanwan@gmail.com>
2015-05-19 09:00:25 +0800

    这个应该可以搞定的

---
+++ binary_search_tree_iterator.cpp
@@ -0,0 +1,38 @@
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class BSTIterator
+{
+public:
+    BSTIterator(TreeNode *root)
+    {
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext()
+    {
+    }
+
+    /** @return the next smallest number */
+    int next()
+    {
+    }
+};
+
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
+
+int main()
+{
+    return 0;
+}
