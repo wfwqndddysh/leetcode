3071de2e5f8fb4de97bb8b704583b2b9403419de
zhangjianjun <zhangfanwan@gmail.com>
2015-05-20 18:49:08 +0800

    利用dummy技术可以减少很多代码，如果感到混乱，就写一个函数，让自己明白此时正在做什么

--- compare_version_numbers.cpp
+++ compare_version_numbers.cpp
@@ -10,42 +10,30 @@ public:
     {
         if(ver1.empty() || ver2.empty()) return 0;
 
-        std::vector<std::string> ver1_s;
-        std::vector<std::string> ver2_s;
         std::vector<int> ver1_n;
         std::vector<int> ver2_n;
 
-        split_to_num(ver1, ver1_s, ver1_n);
-        split_to_num(ver2, ver2_s, ver2_n);
+        split_to_num(ver1, ver1_n);
+        split_to_num(ver2, ver2_n);
 
         size_t i=0, j=0;
-        for(; i<ver1_n.size() && j<ver2_n.size(); ++i, ++j)
+        int num1 = 0;
+        int num2 = 0;
+        for(; i<ver1_n.size() || j<ver2_n.size(); ++i, ++j)
         {
-            if(ver1_n[i]>ver2_n[i])
+            num1 = i<ver1_n.size() ? ver1_n[i] : 0;
+            num2 = j<ver2_n.size() ? ver2_n[j] : 0;
+            
+            if(num1>num2)
                 return 1;
-            else if(ver1_n[i]<ver2_n[i])
+            else if(num1<num2)
                 return -1;
         }
 
-        if(i==ver1_n.size() && j==ver2_n.size())
-            return 0;
-        else if(i==ver1_n.size())
-        {
-            for(; j<ver2_n.size(); ++j)
-                if(ver2_n[j]!=0)
-                    return -1;
-        }
-        else
-        {
-            for(; i<ver1_n.size(); ++i)
-                if(ver1_n[i]!=0)
-                    return 1;
-        }
         return 0;
     }
 
     void split_to_num(const std::string& ver
-            , std::vector<std::string>& ss
             , std::vector<int>& nums)
     {
         if(ver.empty()) return;
@@ -59,25 +47,50 @@ public:
             }
             else
             {
-                ss.push_back(tmp);
-                nums.push_back(get_num(tmp));
+                nums.push_back(std::stoi(tmp));
                 tmp.clear();
             }
         }
         if(!tmp.empty())
         {
-            ss.push_back(tmp);
-            nums.push_back(get_num(tmp));
+            nums.push_back(std::stoi(tmp));
         }
     }
 
-    int get_num(std::string& s)
+    //from leetcode
+    int compareVersion_(const std::string& version1, const std::string& version2)
     {
-        while(!s.empty() && s[0]=='0')
+        int i = 0;
+        int j = 0;
+        int n1 = version1.size();
+        int n2 = version2.size();
+
+        int num1 = 0;
+        int num2 = 0;
+        while(i<n1 || j<n2)
         {
-            s.erase(0, 1);
+            while(i<n1 && version1[i]!='.')
+            {
+                num1 = num1*10+(version1[i]-'0');
+                i++;
+            }
+
+            while(j<n2 && version2[j]!='.')
+            {
+                num2 = num2*10+(version2[j]-'0');;
+                j++;
+            }
+
+            if(num1>num2) return 1;
+            else if(num1 < num2) return -1;
+
+            num1 = 0;
+            num2 = 0;
+            i++;
+            j++;
         }
-        return s.empty() ? 0 : std::stoi(s.c_str());
+
+        return 0;
     }
 };
 
@@ -85,7 +98,7 @@ int main()
 {
     Solution s;
     //std::cout<<s.compareVersion("1.0", "1.1")<<std::endl;
-    std::cout<<s.compareVersion("1.01.1", "1.1.1")<<std::endl;
+    std::cout<<s.compareVersion_("1.01", "1.1")<<std::endl;
     return 0;
 }
 
