6d56e66cc2ee907c0f930481f33ae7c328a9bd51
zhangjianjun <zhangfanwan@gmail.com>
2015-05-21 19:01:06 +0800

    实践一下线性时间排序

--- maximum_gap.cpp
+++ maximum_gap.cpp
@@ -63,16 +63,102 @@ public:
     {
     }
 
-    void count_sort(std::vector<int>& nums)
+    //see introductoin to algorithm
+    std::vector<int> count_sort(const std::vector<int>& A, int max_val)
     {
-        //std::vector<int> 
+        std::vector<int> C(max_val+1, 0);
+        std::vector<int> B(A.size(), 0);
+
+        for(int n : A)
+        {
+            C[n]++;
+        }
+
+        for(size_t i=1; i<C.size(); ++i)
+        {
+            C[i] += C[i-1];
+        }
+
+        for(int i=A.size()-1; i>=0; --i)
+        {
+            B[C[A[i]]-1] = A[i];
+            C[A[i]]--;
+        }
+        return B;
+    }
+
+
+};
+
+
+using namespace std;
+class Solution_
+{
+public:
+    int getMax(vector<int> num){
+        int m = INT_MIN;
+        for (int i=0;i<num.size();i++){
+            m = max(m,num[i]);
+        }
+        return m;
+    }
+     
+    void countSort(vector<int> &num, int nd){
+        int n = num.size();
+        vector<int> output(n,0);
+        vector<int> count(10,0);
+  
+        for (int i = 0; i < n; i++){
+            count[ (num[i]/nd)%10 ]++;
+        }
+  
+        for (int i = 1; i < 10; i++){
+            count[i] += count[i - 1];
+        }
+  
+        for (int i = n - 1; i >= 0; i--){
+            output[ count[(num[i]/nd)%10] - 1] = num[i];
+            count[ (num[i]/nd)%10 ]--;
+        }
+  
+        for (int i = 0; i < n; i++){
+            num[i] = output[i];
+        }
+    }
+ 
+    void radixsort(vector<int> &num){
+         int max_n = getMax(num);
+         for (int nd = 1; max_n/nd > 0; nd *= 10){
+             countSort(num, nd);
+         }
+    }
+     
+    int maximumGap(vector<int> &num) {
+        if (num.size()<2){
+            return 0;
+        }
+        radixsort(num);
+        int res = abs(num[1] - num[0]);
+        for (int i=2;i<num.size();i++){
+            if (num[i]-num[i-1]>res){
+                res = abs(num[i] - num[i-1]);
+            }
+        }
+        return res;
+         
     }
 };
 
 int main()
 {
     Solution s;
-    std::cout<<s.maximumGap({1,10000000})<<std::endl;
+    //std::cout<<s.maximumGap({1,10000000})<<std::endl;
+    std::vector<int> v{1, 123, 2341, 2234, 523, 34, 36, 1234};
+    auto r = s.count_sort(v, 2341);
+    for(int x : r)
+    {
+        std::cout<<x<<std::endl;
+    }
     return 0;
 }
 
