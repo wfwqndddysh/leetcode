584a4a5e5a537fdb8c350078edaa045aff0e550a
zhangjianjun <zhangfanwan@gmail.com>
2015-05-22 18:27:09 +0800

    典型的二分法些法，无法在这里做递归

--- find_minimum_in_rotated_sorted_array_2.cpp
+++ find_minimum_in_rotated_sorted_array_2.cpp
@@ -46,6 +46,32 @@ public:
         }
         return nums[mid];
     }
+
+    int findMin_leetcode(const std::vector<int> &num)
+    {
+        int lo = 0;
+        int hi = num.size() - 1;
+        int mid = 0;
+
+        while(lo < hi)
+        {
+            mid = lo + (hi - lo) / 2;
+
+            if (num[mid] > num[hi])
+            {
+                lo = mid + 1;
+            }
+            else if (num[mid] < num[hi])
+            {
+                hi = mid;
+            }
+            else // when num[mid] and num[hi] are same
+            {
+                hi--;
+            }
+        }
+        return num[lo];
+    }
 };
 
 int main()
