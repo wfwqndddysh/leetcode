63d36a1a4813c28e097313a6c39ed6f1368caef3
zhangjianjun <zhangfanwan@gmail.com>
2015-05-22 09:09:40 +0800

    实现一个bugfree的而分查找

--- find_peak_element.cpp
+++ find_peak_element.cpp
@@ -19,12 +19,42 @@ public:
         }
         return sz-1;
     }
+
+    int findPeakElement_binary(const std::vector<int>& nums)
+    {
+        auto sz = nums.size();
+        if(sz==1) return 0;
+
+        int beg = 0;
+        int end = sz-1;
+        int mid = 0;
+        while(beg<=end)
+        {
+            mid = beg + (end-beg)/2;
+            if(mid==beg && mid==end)
+            {
+                return mid;
+            }
+            else if(mid==beg)
+            {
+            }
+            else if(nums[mid-1]>nums[mid])
+            {
+                end = mid-1;;
+            }
+            else
+            {
+                beg = mid;
+            }
+        }
+        return mid;
+    }
 };
 
 int main()
 {
     Solution s;
-    std::cout<<std::endl;
+    std::cout<<s.findPeakElement_binary({1, 2})<<std::endl;
     return 0;
 }
 
