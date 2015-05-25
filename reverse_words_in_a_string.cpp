2a183cdf04111c792cabd03ae8c6442cedb0e3cd
zhangjianjun <zhangfanwan@gmail.com>
2015-05-25 18:25:01 +0800

    边界条件不算多，不过c++的std::string和c的char*还是有很多不一样的地方

---
+++ reverse_words_in_a_string.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cassert>
+#include<string>
+
+//这个问题的思路在于先reverse整个string,再reverse每个string
+//关于去掉头尾和中间dup的space，维护两个 read/write pointer 就够了
+
+class Solution
+{
+public:
+    void reverseWords(std::string& s)
+    {
+        if(s.empty()) return;
+
+        auto sz=s.size();
+        size_t read = 0;
+        size_t write = 0;
+
+        //reverse while
+        reverseString(s, 0, sz-1, 0);
+
+        //reverse each
+        for(; read<sz; read++)
+        {
+            if(s[read]!=' ')
+            {
+                size_t word_start = read;
+                while(s[read]!=' ' && read<sz) read++;
+                reverseString(s, word_start, read-1, write);
+                write += read-word_start;
+                s[write++] =  ' ';
+            }
+        }
+        for(size_t i=write==0 ? write : write-1; i<sz; ++i)
+        {
+            s.erase(--s.end());
+        }
+        //s[write] =  '\0';
+    }
+
+    void reverseString(std::string& s, size_t beg, size_t end, size_t dest)
+    {
+        if(beg!=dest)
+        {
+            for(size_t i=0; i<end-beg+1; ++i)
+                s[dest+i] = s[beg+i];
+        }
+
+        for(size_t b=dest, e=dest+end-beg; b<e; ++b, --e)
+        {
+            auto tmp = s[e];
+            s[e] = s[b];
+            s[b] = tmp;
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+    //std::string str = " the sky is blue  ";
+    std::string str = " 1";
+    s.reverseWords(str);
+    std::cout<<str.size()<<std::endl;
+    std::cout<<str<<std::endl;
+    return 0;
+}
+
