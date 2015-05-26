64645d573da0dedc2fd79440ea3f52ae353827e0
zhangjianjun <zhangfanwan@gmail.com>
2015-05-26 11:07:27 +0800

    这个应该见过

---
+++ max_points_on_a_line.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cassert>
+#include<vector>
+
+struct Point {
+    int x;
+    int y;
+    Point() : x(0), y(0) {}
+    Point(int a, int b) : x(a), y(b) {}
+};
+
+class Solution
+{
+public:
+    int maxPoints(vector<Point>& points)
+    {
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout<<std::endl;
+    return 0;
+}
+
