c23046b033489178e993f1d51d31b3ec24665157
zhangjianjun <zhangfanwan@gmail.com>
2015-05-26 23:21:50 +0800

    马上就40啦

---
+++ lru_cache.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cassert>
+
+class LRUCache
+{
+public:
+    LRUCache(int capacity)
+    {
+    }
+
+    int get(int key)
+    {
+    }
+
+    void set(int key, int val)
+    {
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout<<std::endl;
+    return 0;
+}
+
+
