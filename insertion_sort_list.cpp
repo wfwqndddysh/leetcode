136e558b3bb942065b510381fc8d407bda8abd35
zhangjianjun <zhangfanwan@gmail.com>
2015-05-26 23:17:13 +0800

    利用dummy方法可以使代码更加简洁

--- insertion_sort_list.cpp
+++ insertion_sort_list.cpp
@@ -74,6 +74,34 @@ private:
         }
         return head;
     }
+
+
+    ListNode* insertionSortListLeetCode(ListNode* head)
+    {
+        if( head == nullptr ){
+            return head;
+        }
+
+        ListNode* helper = new ListNode(0); //new starter of the sorted list
+        ListNode* cur = head; //the node will be inserted
+        ListNode* pre = helper; //insert node between pre and pre.next
+        ListNode* next = nullptr; //the next node will be inserted
+        //not the end of input list
+        while( cur != nullptr ){
+            next = cur->next;
+            //find the right place to insert
+            while( pre->next != nullptr && pre->next->val < cur->val ){
+                pre = pre->next;
+            }
+            //insert between pre and pre.next
+            cur->next = pre->next;
+            pre->next = cur;
+            pre = helper;
+            cur = next;
+        }
+
+        return helper->next;
+    }
 };
 
 int main()
