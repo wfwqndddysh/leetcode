2fd00ef1cf986b00f010da44faccc92d37a2d8d4
zhangjianjun <zhangfanwan@gmail.com>
2015-05-28 00:00:34 +0800

    控制代码的能力提升了，迭代版本肯定可以继续计划代码

--- binary_tree_postorder_traversal.cpp
+++ binary_tree_postorder_traversal.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cassert>
 #include<vector>
+#include<stack>
 
 struct TreeNode
 {
@@ -16,10 +17,62 @@ public:
     std::vector<int> postorderTraversal(TreeNode* root)
     {
         std::vector<int> output;
+        if(!root) return output;
+
+        std::stack<TreeNode*> s;
+        s.push(root);
+
+        while(!s.empty())
+        {
+            while(root->left)
+            {
+                root = root->left;
+                s.push(root);
+            }
+
+            do
+            {
+                if(root->right)
+                {
+                    root = root->right;
+                    s.push(root);
+                    break;
+                }
+                else
+                {
+                    //self
+                    output.push_back(root->val);
+                    s.pop();
+
+                    TreeNode* child = nullptr;
+                    for(child = root; !s.empty(); child=root)
+                    {
+                        root = s.top();
+
+                        if(root->left==child)
+                            break;
+
+                        s.pop();
+                        if(root->right==child)
+                            output.push_back(root->val);
+                    }
+                }
+            } while(!s.empty());
+
+
+        }
+
+        return output;
+    }
+
+    std::vector<int> postorderTraversal_rec(TreeNode* root)
+    {
+        std::vector<int> output;
         rec(root, output);
         return output;
     }
 
+private:
     void rec(TreeNode* root, std::vector<int>& output)
     {
         if(!root)
@@ -34,7 +87,18 @@ public:
 int main()
 {
     Solution s;
-    std::cout<<std::endl;
+    TreeNode a(1);
+    TreeNode b(2);
+    TreeNode c(3);
+
+    //c.right = &a;
+    //a.left = &b;
+
+    c.left = &a;
+    c.right = &b;
+    auto v = s.postorderTraversal(&c);
+    for(int n : v)
+        std::cout<<n<<std::endl;
     return 0;
 }
 
