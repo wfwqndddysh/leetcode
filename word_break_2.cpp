7e5a89f02018153572afd703ad1b314f9af7001b
zhangjianjun <zhangfanwan@gmail.com>
2015-06-01 11:41:52 +0800

    果然是那句话，对于算法设计手册，只是backtrck那一节就值它的价

--- word_break_2.cpp
+++ word_break_2.cpp
@@ -7,127 +7,135 @@
 #include<stack>
 #include<list>
 
+/*
+Backtrack-DFS(A, k)
+    if A = (a1 , a2 , ..., ak ) is a solution, report it.
+    else
+        k = k + 1
+        compute Sk
+        while Sk != ∅ do
+            ak = an element in Sk
+            Sk = Sk − ak
+            Backtrack-DFS(A, k)
+*/
+
+/*
+bool finished = FALSE;
+backtrack(int a[], int k, data input)
+{
+    int c[MAXCANDIDATES];
+    int ncandidates;
+    int i;
+    if (is_a_solution(a,k,input))
+        process_solution(a,k,input);
+    else {
+        k = k+1;
+        construct_candidates(a,k,input,c,&ncandidates);
+        for (i=0; i<ncandidates; i++)
+        {
+            a[k] = c[i];
+            make_move(a,k,input);
+            backtrack(a,k,input);
+            unmake_move(a,k,input);
+            if (finished) return;
+        }
+    }
+}
+*/
+
 class Solution
 {
 public:
     std::vector<std::string> wordBreak(std::string s, const std::unordered_set<std::string> &dict)
     {
-        size_t len = s.length();
-        std::vector<std::string> sentences;
-        std::vector<std::pair<size_t, size_t>> dfs;
-        std::vector<bool> visited(len, false);
-        size_t group_index = 0;
+        s_ = s;
+        dict_= dict;
 
-        std::vector<std::vector<std::list<size_t>>> paths(len);
-        std::list<std::pair<size_t, size_t>> cur_pop_list;
-
-        dfs.push_back({0, 0});
-        while(!dfs.empty())
-        {
-            auto start = dfs.back();
-            dfs.pop_back();
-            cur_pop_list.push_back(start);
-
-            //if(!visited[start])
-            {
-                visited[start.second]=true;
-                size_t cnt = 0;
-                size_t cur_g_i = group_index+1;
-                for(size_t i=start.second; i<len; ++i)
-                {
-                    if(dict.count(s.substr(start.second, i-start.second+1))>0)
-                    {
-                        dfs.push_back({cur_g_i, i+1});
-                        cnt++;
-                        if(i+1==len)
-                        {
-                            //paths[start.second].push_back(std::list<std::size_t>(1, start.second));
-                            for(auto n : cur_pop_list)
-                            {
-                                std::cout<<n.second<<" ";
-                                paths[0].push_back(cur_pop_list);
-                            }
-                            std::cout<<std::endl;
-                        }
-                    }
-                }
-                group_index = cur_g_i > group_index? cur_g_i : group_index;
-                if(cnt==0)
-                {
-                    while(!cur_pop_list.empty() && cur_pop_list.back().first>=dfs.back().first)
-                        cur_pop_list.pop_back();
-                }
-            }
-            /*
-            else
-            {
-                auto index = dfs.back();
-                for(size_t i_paths=0; i_paths<paths[start].size(); ++i_paths)
-                {
-                    paths[index].push_back(paths[start][i_paths]);
-                    paths[index].back().push_front(index);
-                }
-            }
-            */
-        }
-
-        /*
-        int first_non_0 = -1;
-        for(size_t i=0; i<len; ++i)
-        {
-            if(paths[i].size()>0)
-            {
-                first_non_0 = i;
-                break;
-            }
-        }
-        if(first_non_0<0)
-            return sentences;
-
-        const auto& vl = paths[first_non_0];
-        for(size_t i=0; i<vl.size(); ++i)
-        {
-            std::string sentence;
-            for(auto itr=vl[i].cbegin(); itr!=vl[i].cend(); )
-            {
-                if(itr==vl[i].cbegin())
-                {
-                    sentence += s.substr(0, *itr);
-                    sentence += " ";
-                    itr++;
-                }
-                else
-                {
-                    auto ti = itr++;
-                    sentence += s.substr(*ti, *itr-*ti);
-                    sentence += " ";
-                }
-            }
-            sentence += s.substr(vl[i].back());
-            sentences.push_back(sentence);
-        }
-        */
-        return sentences;
+        std::string sentence;
+        backtrack(sentence, 0);
+        return sentences_;
     }
+
+private:
+    void backtrack(std::string& sentence, size_t k)
+    {
+        std::vector<std::string> c;
+
+        if(be_a_solution(sentence, k))
+            process_solution(sentence);
+
+        construct_candidates(k ,c);
+
+        for(size_t i=0; i<c.size(); ++i)
+        {
+            //每次构造了一个新的sentence, 不需要make_move/unmake_move
+            std::string s = sentence+c[i] + " ";
+            backtrack(s, k+c[i].length());
+        }
+    }
+
+    bool be_a_solution(const std::string& c, size_t k)
+    {
+        (void)c;
+        return k==s_.length();
+    }
+
+    void process_solution(std::string& c)
+    {
+        c.erase(--c.end());
+        sentences_.push_back(c);
+    }
+
+    void construct_candidates(size_t k, std::vector<std::string>& candidates)
+    {
+        if(!be_suffix(k))
+            return;
+
+        for(size_t len=1; len<=s_.length()-k; ++len)
+        {
+            const std::string word = s_.substr(k, len);
+            if(dict_.count(word)>0)
+                candidates.push_back(word);
+        }
+    }
+
+    bool be_suffix(size_t k)
+    {
+        for(; k<s_.length(); ++k)
+        {
+            const std::string word = s_.substr(k);
+            if(dict_.count(word)>0)
+                return true;
+        }
+        return false;
+    }
+
+private:
+    std::string s_;
+    std::unordered_set<std::string> dict_;
+    std::vector<std::string> sentences_;
 };
 
 int main()
 {
     Solution s;
-    //std::cout<<s.wordBreak("a", std::unordered_set<std::string>())<<std::endl;
+
     //auto ss="leetcode";
     //std::unordered_set<std::string> m{"leet", "code"};
 
     //auto ss="ab";
     //std::unordered_set<std::string> m{"a", "b"};
 
-    auto ss="catsanddog";
-    std::unordered_set<std::string> m{"cat", "cats", "and", "sand", "dog"};
+    //auto ss="catsanddog";
+    //std::unordered_set<std::string> m{"cat", "cats", "and", "sand", "dog"};
+
+    auto ss = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+    std::unordered_set<std::string> m{"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
 
     std::vector<std::string> ret = s.wordBreak(ss, m);
-    for(const auto& s : ret)
+    for(const auto& sentence : ret)
     {
-        std::cout<<s<<std::endl;
+        std::cout<<sentence<<std::endl;
     }
     return 0;
 }
