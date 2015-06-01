4f9493dd7eb8f133f8d17a867c940aef4bea31ea
zhangjianjun <zhangfanwan@gmail.com>
2015-06-01 21:21:19 +0800

    规规矩矩的做法

--- single_number_2.cpp
+++ single_number_2.cpp
@@ -5,12 +5,34 @@
 class Solution
 {
 public:
-    int singleNumber(const std::vector<int>& nums)
+    int singleNumber(std::vector<int>& nums)
     {
-        assert(nums.size()>0);
+        auto sz = nums.size();
+        assert(sz>0);
+        if(sz==1)
+            return nums[0];
+
+        int bit[32] = {0};
         int ret = 0;
-        for(int n : nums)
-            ret ^= n;
+
+        for(size_t i=0; i<32; ++i)
+        {
+            int cnt_1 = 0;
+            int cnt_0 = 0;
+            for(size_t j=0; j<sz; ++j)
+            {
+                if((nums[j]>>i) & 0x1) cnt_1++;
+                else cnt_0++;
+            }
+
+            bit[i] = (cnt_0 % 3 ==0) ? 1 : 0;
+        }
+
+        for(size_t i=0; i<32; ++i)
+        {
+            ret += bit[i]<<i;
+        }
+
         return ret;
     }
 };
@@ -18,7 +40,8 @@ public:
 int main()
 {
     Solution s;
-    std::cout<<std::endl;
+    std::vector<int> v {2, 2, 3, 2};
+    std::cout<<s.singleNumber(v)<<std::endl;
     return 0;
 }
 
