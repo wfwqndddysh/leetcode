ce73a8fd3eeeb686e676d3a8689296344b6e3dce
zhangjianjun <zhangfanwan@gmail.com>
2015-06-02 22:46:39 +0800

    其实根本不用环绕的，不过一般人想不到这个

--- gas_station.cpp
+++ gas_station.cpp
@@ -27,6 +27,22 @@ public:
         }
         return -1;
     }
+
+    int canCompleteCircuitLeetCode(std::vector<int>& gas, std::vector<int>& cost)
+    {
+        int start(0),total(0),tank(0);
+        //if car fails at 'start', record the next station
+        for(int i=0;i<gas.size();i++)
+        {
+            if((tank=tank+gas[i]-cost[i])<0)
+            {
+                start=i+1;
+                total+=tank;
+                tank=0;
+            }
+        }
+        return (total+tank<0) ? -1 : start;
+    }
 };
 
 int main()
