f7807f36f741816f697396470153f3d9022b698e
zhangjianjun <zhangfanwan@gmail.com>
2015-06-02 15:11:28 +0800

    medium,应该不难，代码简短

---
+++ gas_station.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cassert>
+#include<vector>
+
+class Solution
+{
+public:
+    int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost)
+    {
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout<<std::endl;
+    return 0;
+}
+
