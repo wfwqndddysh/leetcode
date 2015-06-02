0eeadee73a2d329435950ecde26b0cb782d902c5
zhangjianjun <zhangfanwan@gmail.com>
2015-06-02 18:38:57 +0800

    好像不难

--- gas_station.cpp
+++ gas_station.cpp
@@ -7,6 +7,32 @@ class Solution
 public:
     int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost)
     {
+        int sz=gas.size();
+
+        //find first gas
+        int start = -1;
+        for(int i=0; i<sz; ++i)
+        {
+            if(gas[i]-cost[i]>=0)
+            {
+                start = i;
+                break;
+            }
+        }
+        if(start==-1) return -1;
+
+
+        int cur_left = gas[start]-cost[start];
+
+        for(int i=start+1; start<sz; ++i)
+        {
+            if(gas[i]-cost[i]+cur_left<0)
+            {
+                start = i+1;
+                cur_left = 0;
+            }
+        }
+        return start;
     }
 };
 
