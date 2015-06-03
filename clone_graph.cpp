d5585acdbe247a190e10af64a64b2c6256142935
zhangjianjun <zhangfanwan@gmail.com>
2015-06-03 08:55:01 +0800

    先用hash实现

--- clone_graph.cpp
+++ clone_graph.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cassert>
 #include<vector>
+#include<unordered_map>
 
 struct UndirectedGraphNode
 {
@@ -14,6 +15,21 @@ class Solution
 public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node)
     {
+    }
+private:
+    void label2loc(UndirectedGraphNode* node
+            , int deep
+            , int index
+            , std::unordered_map<int, std::pair<int, int>> label_loc_map)
+    {
+        if(label_loc_map.count(node->label)>0)
+            return;
+
+        label_loc_map.insert({node->label, {deep, index}});
+        for(size_t i=0; i<node->neighbors.size(); ++i)
+        {
+            label2loc(node->neighbors[i], deep+1, i, label_loc_map);
+        }
     }
 };
 
