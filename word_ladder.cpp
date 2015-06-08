a2bdfd77c8fc9db00b35290ae6ef82ba41a5f3fe
zhangjianjun <zhangfanwan@gmail.com>
2015-06-08 18:16:11 +0800

    总结思考一些算法方法

--- word_ladder.cpp
+++ word_ladder.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 #include<cassert>
+#include<string>
+#include<unordered_set>
 
 class Solution
 {
 public:
+    int ladderLength(const std::string& beginWord
+            , const std::string& endWord
+            , const std::unordered_set<std::string>& wordDict)
+    {
+        int min_length = 0;
+        return min_length;
+    }
 };
 
 int main()
