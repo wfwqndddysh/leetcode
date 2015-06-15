d7dce1fd5b1e5d2f48024c7174715f8d4f63f975
zhangjianjun <zhangfanwan@gmail.com>
2015-06-15 21:27:36 +0800

    so easy

---
+++ binary_tree_level_order_traversal.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<cassert>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> levelOrderBottom(TreeNode* root)
+    {
+        std::vector<std::vector<int>> res;
+        if(!root) return res;
+
+        std::queue<TreeNode*> q;
+        q.push(root);
+        res.push_back({});
+
+        int cnt_last_level(1);
+        int cnt_cur_level(0);
+        while(!q.empty())
+        {
+            auto cur = q.front();
+            q.pop();
+
+            if(cur->left)
+            {
+                q.push(cur->left);
+                cnt_cur_level++;
+            }
+            if(cur->right)
+            {
+                q.push(cur->right);
+                cnt_cur_level++;
+            }
+
+            res.back().push_back(cur->val);
+
+            if(--cnt_last_level==0)
+            {
+                cnt_last_level=cnt_cur_level;
+                cnt_cur_level=0;
+                res.push_back({});
+            }
+        }
+
+        if(res.back().empty()) res.pop_back();
+        return res;
+    }
+};
+
+class SolutionRec
+{
+public:
+    std::vector<std::vector<int>> levelOrderBottom(TreeNode* root)
+    {
+        std::vector<std::vector<int>> res;
+        rec(root, 0, res);
+        return res;
+    }
+private:
+    void rec(TreeNode* root, size_t level, std::vector<std::vector<int>>& res)
+    {
+        if(!root) return;
+
+        if(level==res.size())
+            res.push_back({});
+
+        res[level].push_back(root->val);
+
+        rec(root->left, level+1, res);
+        rec(root->right, level+1, res);
+    }
+};
+
+int main()
+{
+    Solution s;
+    TreeNode a(1);
+    s.levelOrderBottom(&a);
+    std::cout<<std::endl;
+    return 0;
+}
+
