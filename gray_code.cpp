658ec883a368ee46d203a6360a9ec0ecaa21bf3a
zhangjianjun <zhangfanwan@gmail.com>
2015-06-18 18:58:53 +0800

    vector由后向前遍历，每次增加1<<i(0<=i<n),都想到了，如果按照这个思路枚举出n=4的情况，肯定看到了规律了，所以关于二进制的问题，一定要枚举出来

--- gray_code.cpp
+++ gray_code.cpp
@@ -7,12 +7,41 @@ class Solution
 public:
     std::vector<int> grayCode(int n)
     {
+        std::vector<int> v(1,0);
+        for (int i=1;i<(1<<n);i++) v.push_back(v[i-1]^(i&-i));
+        return v;
     }
 };
 
+class SolutionFor
+{
+public:
+    std::vector<int> grayCode(int n)
+    {
+        std::vector<int> result(1, 0);
+        for (int i = 0; i < n; i++)
+        {
+            int sz = result.size();
+            for(int j=sz-1; j>=0; --j)
+            {
+                int cur = result[j];
+                cur += (1<<i);
+                result.push_back(cur);
+            }
+        }
+        return result;
+    }
+};
+
+
 int main()
 {
-    Solution s;
+    SolutionFor s;
+    auto v = s.grayCode(4);
+
+    for(int n : v)
+        std::cout<<n<<std::endl;
+
     std::cout<<std::endl;
     return 0;
 }
