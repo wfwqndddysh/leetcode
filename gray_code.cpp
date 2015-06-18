a660c64c29c855c8e53a89b4275f79610a59d99b
zhangjianjun <zhangfanwan@gmail.com>
2015-06-18 14:41:41 +0800

    这个问题应该比较简单

---
+++ gray_code.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cassert>
+#include<vector>
+
+class Solution
+{
+public:
+    std::vector<int> grayCode(int n)
+    {
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout<<std::endl;
+    return 0;
+}
+
