1df42c1c0d7d12bb3e93df47b34f060879b9a87f
zhangjianjun <zhangfanwan@gmail.com>
2015-06-25 17:07:09 +0800

    其实想到了这个思路，觉得控制代码比较繁琐，而且觉得可能有更统一的方法，所以没有尝试

--- permutation_sequence.cpp
+++ permutation_sequence.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cassert>
 #include<vector>
+#include<algorithm>
 
 class Solution
 {
@@ -50,11 +51,85 @@ private:
     bool found_;
 };
 
+class SolutionNext
+{
+public:
+    std::string getPermutation(int n, int k)
+    {
+        std::string perm;
+        if(k<=0) return perm;
+
+        for(int i=1; i<=n; ++i)
+        {
+            perm.push_back(i-0+'0');
+        }
+
+        for(int i=2; i<=k; ++i)
+        {
+            int j=n-2;
+            for(; j>=0; --j)
+            {
+                if(perm[j]<perm[j+1]) break;
+            }
+
+            if(j==-1) break;
+
+            int min_i=-1;
+            char min = '9'+1;
+            for(int m=n-1; m>j; --m)
+            {
+                if(perm[m]>perm[j] && perm[m]<min)
+                {
+                    min_i = m;
+                    min = perm[m];
+                }
+            }
+
+            std::swap(perm[j], perm[min_i]);
+
+            std::reverse(perm.begin()+j+1, perm.end());
+        }
+
+        return perm;
+    }
+};
+
+class SolutionLeetCode
+{
+public:
+    std::string getPermutation(int n, int k)
+    {
+        int i,j,f=1;
+        //left part of s is partially formed permutation, right part is the leftover chars.
+        std::string s(n,'0');
+        for(i=1;i<=n;i++)
+        {
+            f*=i;
+            s[i-1]+=i; // make s become 1234...n
+        }
+        for(i=0,k--;i<n;i++)
+        {
+            f/=n-i;
+            j=i+k/f; // calculate index of char to put at s[i]
+            char c=s[j];
+            // remove c by shifting to cover up (adjust the right part).
+            for(;j>i;j--)
+                s[j]=s[j-1];
+            k%=f;
+            s[i]=c;
+        }
+        return s;
+    }
+};
+
 int main()
 {
     Solution s;
+    SolutionNext sn;
+    SolutionLeetCode sl;
     //std::cout<<s.getPermutation(3, 4)<<std::endl;
-    std::cout<<s.getPermutation(9, 331987)<<std::endl;
+    //std::cout<<s.getPermutation(9, 331987)<<std::endl;
+    std::cout<<sl.getPermutation(8, 8590)<<std::endl;
     return 0;
 }
 
