424056bde7f48a1ab45bd81eff576da307bbde34
zhangjianjun <zhangfanwan@gmail.com>
2015-06-30 11:23:09 +0800

    这个实现完美的令人尖叫

--- multiply_strings.cpp
+++ multiply_strings.cpp
@@ -63,6 +63,30 @@ public:
     }
 };
 
+class SolutionLeetCode
+{
+public:
+    std::string multiply(std::string num1, std::string num2) {
+        std::string sum(num1.size() + num2.size(), '0');
+
+        for (int i = num1.size() - 1; 0 <= i; --i) {
+            int carry = 0;
+            for (int j = num2.size() - 1; 0 <= j; --j) {
+                int tmp = (sum[i + j + 1] - '0') + (num1[i] - '0') * (num2[j] - '0') + carry;
+                sum[i + j + 1] = tmp % 10 + '0';
+                carry = tmp / 10;
+            }
+            sum[i] += carry;
+        }
+
+        size_t startpos = sum.find_first_not_of("0");
+        if (std::string::npos != startpos) {
+            return sum.substr(startpos);
+        }
+        return "0";
+    }
+};
+
 int main()
 {
     Solution s;
