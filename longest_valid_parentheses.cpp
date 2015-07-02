db351ddd7778bf39af11a19fad8d92dbe78a1a32
zhangjianjun <zhangfanwan@gmail.com>
2015-07-02 23:04:17 +0800

    速度虽然不是很快，因为用到了stack，vector，甚至还有sort,不过这说明自己可以创造一些算法了, 不错

--- longest_valid_parentheses.cpp
+++ longest_valid_parentheses.cpp
@@ -2,6 +2,8 @@
 #include<cassert>
 #include<string>
 #include<stack>
+#include<vector>
+#include<algorithm>
 
 class Solution
 {
@@ -11,32 +13,19 @@ public:
         int sz=s.size();
         if(sz==0) return 0;
 
-        int cur=0;
         int longest=0;
-
-        int beg=-1;
-        int end=-1;
-
+        int new_longest=0;
+        std::vector<std::pair<int, int>> ranges;
         std::stack<std::pair<char, int>> st;
-        st.push({s[sz-1], sz-1});
 
-        for(int i=sz-2; i>=0; --i)
+        for(int i=0; i<sz; ++i)
         {
-            if(s[i]=='(')
+            if(s[i]==')')
             {
                 if(!st.empty())
                 {
-                    cur_end=st.top().second;
+                    ranges.push_back({st.top().second, i});
                     st.pop();
-                    if()
-                    {
-                    }
-                    cur+=2;
-                }
-                else
-                {
-                    longest=std::max(longest, cur);
-                    cur=0;
                 }
             }
             else
@@ -45,7 +34,30 @@ public:
             }
         }
 
-        return std::max(cur, longest);
+        auto comp=[](const std::pair<int, int>& l, const std::pair<int, int>& r)
+            { return l.first<r.first; };
+
+        std::sort(ranges.begin(), ranges.end(), comp);
+
+        //合并range
+        if(ranges.size()>0) longest=new_longest=ranges[0].second-ranges[0].first+1;
+        int prev=0;
+        for(size_t i=1; i<ranges.size(); ++i)
+        {
+            if(ranges[prev].second+1==ranges[i].first)
+            {
+                new_longest += ranges[i].second-ranges[i].first+1;
+                prev=i;
+            }
+            else if(ranges[prev].second+1<ranges[i].first)
+            {
+                longest = std::max(longest, new_longest);
+                new_longest = ranges[i].second-ranges[i].first+1;
+                prev=i;
+            }
+        }
+
+        return std::max(longest, new_longest);
     }
 };
 
@@ -54,10 +66,10 @@ int main()
     Solution s;
     //std::string parentheses("()(()");
     //std::string parentheses("()");
-    //std::string parentheses("()()");
+    std::string parentheses("()()");
     //std::string parentheses("()()");
     //std::string parentheses("(()");
-    std::string parentheses(")()())()()(");
+    //std::string parentheses(")()())()()(");
     std::cout<<s.longestValidParentheses(parentheses)<<std::endl;
     return 0;
 }
