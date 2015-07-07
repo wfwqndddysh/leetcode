bb89976861b665510acab5da86baecf53d9c5e24
zhangjianjun <zhangfanwan@gmail.com>
2015-07-07 10:18:05 +0800

    对于unordered_map还是不够熟练

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,29 +1,80 @@
 #include<iostream>
 #include<cassert>
 #include<vector>
+#include<algorithm>
+#include<unordered_map>
 
 class Solution
 {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
-        std::vector<int> res;
-        for(size_t i=0; i<nums.size()-1; ++i)
+        std::vector<std::pair<int, int>> cp;
+
+        for(int i=0; i<(int)nums.size(); ++i)
         {
-            for(size_t j=0; j<nums.size(); ++j)
+            cp.push_back({nums[i], i});
+        }
+
+        auto comp=[](const std::pair<int, int>& l, const std::pair<int, int>& r)
+            { return l.first<r.first;};
+
+        std::sort(cp.begin(), cp.end(), comp);
+        std::vector<int> res;
+        for(int i=0; i<(int)cp.size()-1; ++i)
+        {
+            int second=find_second(cp, i+1, target-cp[i].first);
+            if(second>=0)
             {
-                if(nums[i]+nums[j]==target)
-                    return {(int)i+1, (int)j+1};
+                if(cp[i].second<second)
+                    return {cp[i].second+1, second+1};
+                else
+                    return {second+1, cp[i].second+1};
             }
         }
 
         return res;
+    }
+
+private:
+    int find_second(const std::vector<std::pair<int, int>>& nums, int start, int target)
+    {
+        int low = start;
+        int high = nums.size()-1;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            if(nums[mid].first<target)
+            {
+                low=mid+1;
+            }
+            else if(nums[mid].first==target)
+            {
+                return nums[mid].second;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+
+        return -1;
     }
 };
 
 int main()
 {
     Solution s;
+    /*
+    std::vector<int> nums{3, 2, 4};
+    s.twoSum(nums, 6);
+    */
+
+    std::vector<int> nums{150,24,79,50,88,345,3};
+    s.twoSum(nums, 200);
+
     std::cout<<std::endl;
     return 0;
 }
