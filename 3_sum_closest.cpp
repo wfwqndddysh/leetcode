310f69a19b264c14c16205593211b396087d7c04
zhangjianjun <zhangfanwan@gmail.com>
2015-07-08 11:34:06 +0800

    这个问题时因为那个算法是有问题的，front，back都不能移动并不意味着后面没有更好的解

--- 3_sum_closest.cpp
+++ 3_sum_closest.cpp
@@ -4,73 +4,84 @@
 #include<algorithm>
 #include<climits>
 
-class Solution
+class SolutionLeetCode
 {
 public:
     int threeSumClosest(std::vector<int>& nums, int target)
     {
-        int sz=nums.size();
-        std::sort(nums.begin(), nums.end());
+        std::vector<int> v(nums.begin(), nums.end()); // I didn't wanted to disturb original array.
+        int n = 0;
+        int ans = 0;
+        int sum;
 
-        int diff=INT_MAX;
-        for(int i=0; i<sz-2; )
+        sort(v.begin(), v.end());
+
+        // If less then 3 elements then return their sum
+        while (v.size() <= 3)
         {
-            int target2=target-nums[i];
-
-            int front=i+1;
-            int back=sz-1;
-
-            diff = abs(target2-nums[front]-nums[back]) < abs(diff) ? abs(target2-nums[front]-nums[back]) : diff;
-
-            while(front+1<back)
-            {
-                int f=front;
-                int b=back;
-
-                int i=0;
-                for (int new_diff=diff; abs(new_diff)<=abs(diff) && front+i+1<back; i++)
-                {
-                    diff=new_diff;
-                    new_diff=target2-nums[front+i+1]-nums[back];
-                }
-                front+=i-1;
-
-                i=0;
-                for (int new_diff=diff; abs(new_diff)<=abs(diff) && front<back-i-1; ++i)
-                {
-                    diff=new_diff;
-                    new_diff=target2-nums[front]-nums[back-i-1];
-                }
-                back-=i-1;
-
-                if(f==front && b==back) break;
-            }
-            for(i++; nums[i]==nums[i-1] && i<sz-2; ++i);
+            return accumulate(v.begin(), v.end(), 0);
         }
 
-        return target-diff;
+        n = v.size();
+
+        /* v[0] v[1] v[2] ... v[i] .... v[j] ... v[k] ... v[n-2] v[n-1]
+         *                    v[i]  <=  v[j]  <= v[k] always, because we sorted our array. 
+         * Now, for each number, v[i] : we look for pairs v[j] & v[k] such that 
+         * absolute value of (target - (v[i] + v[j] + v[k]) is minimised.
+         * if the sum of the triplet is greater then the target it implies
+         * we need to reduce our sum, so we do K = K - 1, that is we reduce
+         * our sum by taking a smaller number.
+         * Simillarly if sum of the triplet is less then the target then we
+         * increase out sum by taking a larger number, i.e. J = J + 1.
+         */
+        ans = v[0] + v[1] + v[2];
+        for (int i = 0; i < n-2; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                sum = v[i] + v[j] + v[k];
+                if (abs(target - ans) > abs(target - sum)) {
+                    ans = sum;
+                    if (ans == target) return ans;
+                }
+                (sum > target) ? k-- : j++;
+            }
+        }
+        return ans;
     }
 };
 
 int main()
 {
-    Solution s;
+    SolutionLeetCode s;
 
     /*
     std::vector<int> nums{0, 0, 0};
     int sum=s.threeSumClosest(nums, 1);
     */
 
-    //std::vector<int> nums{1,2,4,8,16,32,64,128};
-    //int sum=s.threeSumClosest(nums, 82);
+    /*
+    std::vector<int> nums{1,2,4,8,16,32,64,128};
+    int sum=s.threeSumClosest(nums, 82);
+    */
 
     /*
     std::vector<int> nums{1, 1, 1, 0};
     int sum=s.threeSumClosest(nums, 100);
     */
 
+    /*
     std::vector<int> nums{1, 1, 1, 0};
     int sum=s.threeSumClosest(nums, -100);
+    */
+
+    /*
+    std::vector<int> nums{0, 2, 1, -3};
+    int sum=s.threeSumClosest(nums, 0);
+    */
+
+    std::vector<int> nums{13,2,0,-14,-20,19,8,-5,-13,-3,20,15,20,5,13,14,-17,-7,12,-6,0,20,-19,-1,-15,-2,8,-2,-9,13,0,-3,-18,-9,-9,-19,17,-14,-19,-4,-16,2,0,9,5,-7,-4,20,18,9,0,12,-1,10,-17,-11,16,-13,-14,-3,0,2,-18,2,8,20,-15,3,-13,-12,-2,-19,11,11,-10,1,1,-10,-2,12,0,17,-19,-7,8,-19,-17,5,-5,-10,8,0,-12,4,19,2,0,12,14,-9,15,7,0,-16,-5,16,-12,0,2,-16,14,18,12,13,5,0,5,6};
+    int sum=s.threeSumClosest(nums, -59);
 
     std::cout<<sum<<std::endl;
     return 0;
