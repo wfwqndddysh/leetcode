aeb80cf52be3e23c0a9f5a2bd52b5f5724924db7
zhangjianjun <zhangfanwan@gmail.com>
2015-07-09 22:50:26 +0800

    没有想想中可怕，而且居然不要处理16进制

--- string_to_integer_atoi.cpp
+++ string_to_integer_atoi.cpp
@@ -79,6 +79,82 @@ public:
     }
 };
 
+/*
+居然不用考虑16进制
+*/
+class Solution_
+{
+public:
+    int myAtoi(std::string str)
+    {
+        if(str.empty()) return 0;
+
+        int len=str.size();
+        int64_t res=0;
+        int sign=1;
+        int base=10;
+        int start=0;;
+
+        while(start<len && (str[start]==' ' || str[start]=='\t'))
+            start++;
+
+        if(str[start]=='+')
+        {
+            sign=1;
+            start++;
+        }
+        else if(str[start]=='-')
+        {
+            sign=-1;
+            start++;
+        }
+
+        for(int i=start; i<len; ++i)
+        {
+            if(str[i]=='0' && res==0)
+                continue;
+
+            if(str[i]<'0' || str[i]>'9')
+                break;
+
+            res = res*base + str[i]-'0';
+
+            int64_t cur=res*sign;
+            if(cur>INT_MAX) return INT_MAX;
+            if(cur<INT_MIN) return INT_MIN;
+        }
+
+        return res*sign;
+    }
+};
+
+//leetcode
+//对溢出的另一种处理手法
+int atoi(const char *str) {
+
+    int sign = 1, base = 0, i = 0;
+
+    while (str[i] == ' ')
+        i++;
+
+    if (str[i] == '-' || str[i] == '+')
+    {
+        sign = 1 - 2 * (str[i++] == '-'); 
+    }
+
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7))
+        {
+            if (sign == 1) return INT_MAX;
+            else return INT_MIN;
+        }
+
+        base  = 10 * base + (str[i++] - '0');
+    }
+    return base * sign;
+}
+
 int main()
 {
     Solution s;
