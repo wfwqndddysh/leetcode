b6f3181278226820a44e56c27cfab8dd5df2920a
zhangjianjun <zhangfanwan@gmail.com>
2015-07-10 23:12:08 +0800

    这个算法细节还是挺多的，这里需要学习的地方也不少，比如通过加字符来避免偶数奇数求两遍的局面，同时还是避免越界检查

--- longest_palindromic_substring.cpp
+++ longest_palindromic_substring.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cassert>
+#include<vector>
 
 class Solution
 {
@@ -56,11 +57,63 @@ public:
     }
 };
 
+class SolutionLeetCode
+{
+public:
+    std::string longestPalindrome(const std::string& s)
+    {
+        if(s.size()<2) return s;
+
+        std::string tmp("?");
+        for(size_t i=0; i<s.length(); ++i)
+        {
+            tmp.push_back(s[i]);
+            tmp.push_back('#');
+        }
+        int len=tmp.size();
+        tmp[len-1]='*';
+
+        int longest=0;
+        int start=-1;
+        std::vector<int> rad(len, 0);
+        for (int i=1,j=0,k; i<len; )
+        {
+            while (tmp[i-j-1]==tmp[i+j+1] )
+            {
+                j++;  //扫描得出rad值
+            }
+
+            rad[i]=j;
+            if(j>longest || (j==longest && tmp[i]=='#'))
+            {
+                longest=j;
+                start=i-j;
+            }
+            for (k=1; k<=j && rad[i-k]!=rad[i]-k; k++)
+            {
+                rad[i+k]=std::min(rad[i-k],rad[i]-k);  //k指针扫描
+            }
+
+            i+=k;  //i跳到下一个需要计算rad值的位置
+            j=std::max(j-k,0);  //更新下一个rad值的初始值
+        }
+
+        std::string res;
+        for(int i=0; i<=2*longest; ++i)
+            if(tmp[i+start]!='#')
+                res.push_back(tmp[i+start]);
+
+        return res;
+    }
+};
+
 int main()
 {
-    Solution s;
+    SolutionLeetCode s;
     //std::cout<<s.longestPalindrome("aaaa")<<std::endl;
-    std::cout<<s.longestPalindrome("bb")<<std::endl;
+    //std::cout<<s.longestPalindrome("bb")<<std::endl;
+    //std::cout<<s.longestPalindrome("ccc")<<std::endl;
+    std::cout<<s.longestPalindrome("abb")<<std::endl;
     return 0;
 }
 
