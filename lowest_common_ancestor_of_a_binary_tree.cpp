a02ba25716b3bd708f86233ab9a06b07012a21a4
zhangjianjun <zhangfanwan@gmail.com>
2015-07-14 10:50:12 +0800

    就是说嘛，怎么会有重复的情况

--- lowest_common_ancestor_of_a_binary_tree.cpp
+++ lowest_common_ancestor_of_a_binary_tree.cpp
@@ -16,77 +16,22 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
         if(!root) return nullptr;
-        assert(p && q);
 
-        std::stack<TreeNode*> ancestor;
-
-        //find p or q
-        TreeNode* one=find_first(root, p, q, ancestor);
-        auto another=one->val==p->val ? q : p;
-
-        if(contains(one->left, another) || contains(one->right, another)) return one;
-
-        while(!ancestor.empty())
-        {
-            TreeNode* a=ancestor.top();
-
-            if(a->right!=one)
-            {
-                std::stack<TreeNode*> ancestor_next;
-                auto next=find_first(a->right, p, q, ancestor_next);
-                if(next->val==another->val) return a;
-                else
-                {
-                    ancestor=std::move(ancestor_next);
-                    continue;
-                }
-            }
-
-            one=a;
-            ancestor.pop();
-        }
-
-        return nullptr;
-    }
-
-private:
-    TreeNode* find_first(TreeNode* root, TreeNode* p, TreeNode* q, std::stack<TreeNode*>& ancestor)
-    {
-        if(!root) return nullptr;
-        if(p->val==root->val || q->val==root->val)
-        {
+        if(root==p || root==q)
             return root;
-        }
-        else
-        {
-            ancestor.push(root);
-            size_t sz=ancestor.size();
-            TreeNode* t=find_first(root->left, p, q, ancestor);
-            if(t)
-            {
-                return t;
-            }
-            else
-            {
-                while(ancestor.size()>sz) ancestor.pop();
-                return find_first(root->right, p, q, ancestor);
-            }
-        }
-    }
 
-    bool contains(TreeNode* root, TreeNode* another)
-    {
-        if(!root) return false;
-        return root->val==another->val || contains(root->left, another) || contains(root->right, another);
+        TreeNode* l=lowestCommonAncestor(root->left, p, q);
+        TreeNode* r=lowestCommonAncestor(root->right, p, q);
+
+        if(l && r) return root;
+
+        return l ? l : r;
     }
 };
 
 int main()
 {
     Solution s;
-
-    [37,-34,-48,null,-100,-100,48,null,null,null,null,-54,null,-71,-22,null,null,null,8], node with value -100, node with value -71
-
     std::cout<<std::endl;
     return 0;
 }
