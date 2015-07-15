1a952fadc5788d0858da2a3437cba7f8e8d7e8d9
zhangjianjun <zhangfanwan@gmail.com>
2015-07-15 11:55:01 +0800

    上一个问题为这个问题提供了不少思路

---
+++ basic_calculator_2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cassert>
+#include<string>
+#include<stack>
+
+class Solution
+{
+public:
+    int calculate(const std::string& s)
+    {
+    }
+};
+
+int main()
+{
+    Solution s;
+    //std::cout<<s.calculate("1")<<std::endl;
+    //std::cout<<s.calculate("(1)")<<std::endl;
+    //std::cout<<s.calculate("(4+9)")<<std::endl;
+    //std::cout<<s.calculate("1+1")<<std::endl;
+    //std::cout<<s.calculate("1-(5)")<<std::endl;
+    //std::cout<<s.calculate("5  ")<<std::endl;
+    std::cout<<s.calculate("(3)+1")<<std::endl;
+    return 0;
+}
+
