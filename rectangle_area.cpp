0faa2fefc451abc92121505c63486c079ed0fcad
zhangjianjun <zhangfanwan@gmail.com>
2015-07-16 09:16:33 +0800

    细节题

---
+++ rectangle_area.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cassert>
+
+class Solution
+{
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+    {
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout<<std::endl;
+    return 0;
+}
+
