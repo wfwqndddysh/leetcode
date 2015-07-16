b30fdadaba21a36c3e30dc808b1342384d065a08
zhangjianjun <zhangfanwan@gmail.com>
2015-07-16 09:57:31 +0800

    easy, 提交了两次

--- rectangle_area.cpp
+++ rectangle_area.cpp
@@ -4,8 +4,20 @@
 class Solution
 {
 public:
-    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+    int computeArea(int a, int b, int c, int d, int e, int f, int g, int h)
     {
+        //(a,b),(c,d)
+        //(e,f),(g,h)
+
+        int area1=(c-a)*(d-b);
+        int area2=(g-e)*(h-f);
+
+        if(a>=g || e>=c || b>=h || f>=d ) return area1+area2;
+
+        int x=std::min(c, g)-std::max(a, e);
+        int y=std::min(d, h)-std::max(b, f);
+
+        return area1+area2-x*y;
     }
 };
 
