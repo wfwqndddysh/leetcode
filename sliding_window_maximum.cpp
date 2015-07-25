ca05fa8f370a0203c31297b0f2f8d41adaf36f08
zhangjianjun <zhangfanwan@gmail.com>
2015-07-25 17:30:32 +0800

    双端队列的应用

--- sliding_window_maximum.cpp
+++ sliding_window_maximum.cpp
@@ -15,52 +15,6 @@ public:
         if(sz==0) return {};
 
         std::vector<int> res;
-        std::vector<std::pair<int, int>> heap;
-
-        for(int i=0; i<k; ++i)
-        {
-            heap.push_back({nums[i], i});
-        }
-
-        auto comp=[](const std::pair<int, int>& l, const std::pair<int, int>& r)
-        {
-            return l.first<r.first;
-        };
-
-        std::make_heap(heap.begin(), heap.end(), comp);
-        res.push_back(heap.front().first);
-
-        heap.push_back({nums[k], k});
-        auto last=heap.size();
-
-        for(int i=k; i<sz; ++i)
-        {
-            if(last==heap.size()) heap.resize(heap.size()+1);
-
-            heap[last++]={nums[i], i};
-            std::make_heap(heap.begin(), heap.begin()+last);
-
-            while(heap[0].second<i-k)
-            {
-                std::pop_heap(heap.begin(), heap.begin()+last--);
-            }
-
-            res.push_back(heap[0].first);
-        }
-
-        return res;
-    }
-};
-
-class Solution_
-{
-public:
-    std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k)
-    {
-        int sz=nums.size();
-        if(sz==0) return {};
-
-        std::vector<int> res;
         std::multimap<int, int> heap;
 
         for(int i=0; i<k; ++i)
@@ -109,39 +63,27 @@ public:
         if(sz==0) return {};
 
         std::vector<int> res;
-        std::multimap<int, int> heap;
+        std::deque<std::pair<int, int>> heap;
+        heap.push_back({INT_MIN, -1});
 
         for(int i=0; i<k; ++i)
         {
-            heap.insert({nums[i], i});
+            while(!heap.empty() && nums[i]>=heap.back().first)
+                heap.pop_back();
+            heap.push_back({nums[i], i});
         }
 
-        auto cur_max=--heap.end();
-        res.push_back(cur_max->first);
+        res.push_back(heap.front().first);
+        if(heap.front().second==0) heap.pop_front();
 
         for(int i=k; i<sz; ++i)
         {
-            if(cur_max->second<=i-k)
-            {
-                heap.erase(cur_max);
-                heap.insert({nums[i], i});
-                cur_max=--heap.end();
-                res.push_back(cur_max->first);
-            }
-            else
-            {
-                if(cur_max->first>nums[i])
-                {
-                    res.push_back(cur_max->first);
-                }
-                else
-                {
-                    res.push_back(nums[i]);
-                }
-                heap.erase(heap.find(nums[i-k]));
-                heap.insert({nums[i], i});
-                cur_max=--heap.end();
-            }
+            while(!heap.empty() && nums[i]>=heap.back().first)
+                heap.pop_back();
+            heap.push_back({nums[i], i});
+
+            res.push_back(heap.front().first);
+            if(i-heap.front().second==k-1) heap.pop_front();
         }
 
         return res;
@@ -150,7 +92,8 @@ public:
 
 int main()
 {
-    Solution_ s;
+    //Solution s;
+    SolutionDeque s;
 
     //std::vector<int> nums{1,3,-1,-3,5,3,6,7};
     //auto v=s.maxSlidingWindow(nums, 3);
