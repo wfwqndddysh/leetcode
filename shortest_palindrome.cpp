60666949e84a8e76e10fd6d0128fd8827af5f52e
zhangjianjun <zhangfanwan@gmail.com>
2015-07-27 15:19:43 +0800

    对manacher算法进行改造

--- shortest_palindrome.cpp
+++ shortest_palindrome.cpp
@@ -51,12 +51,69 @@ private:
     }
 };
 
+class Solution_
+{
+public:
+    std::string shortestPalindrome(std::string s)
+    {
+        int sz=s.size();
+        if(sz<2) return s;
+
+        int longest_from_head=longestPalindromeHead(s);
+        std::string min_s=s.substr(longest_from_head);
+        std::reverse(min_s.begin(), min_s.end());
+        return min_s+s;
+    }
+
+private:
+    int longestPalindromeHead(const std::string& s)
+    {
+        if(s.size()<2) return s.size();
+        int res=0;
+
+        std::string tmp("?");
+        for(size_t i=0; i<s.length(); ++i)
+        {
+            tmp.push_back(s[i]);
+            tmp.push_back('#');
+        }
+        int len=tmp.size();
+        tmp[len-1]='*';
+
+        std::vector<int> rad(len, 0);
+        for (int i=1,j=0,k; i<len; )
+        {
+            while (tmp[i-j-1]==tmp[i+j+1] )
+            {
+                j++;  //扫描得出rad值
+            }
+
+            rad[i]=j;
+            if(i-j==1)
+            {
+                res=std::max(res, j+1);
+            }
+            for (k=1; k<=j && rad[i-k]!=rad[i]-k; k++)
+            {
+                rad[i+k]=std::min(rad[i-k],rad[i]-k);  //k指针扫描
+            }
+
+            i+=k;  //i跳到下一个需要计算rad值的位置
+            j=std::max(j-k,0);  //更新下一个rad值的初始值
+        }
+
+        return res;
+    }
+};
+
 int main()
 {
-    Solution s;
+    Solution_ s;
 
-    std::cout<<s.shortestPalindrome("aacecaaa")<<std::endl;
-    std::cout<<s.shortestPalindrome("abcd")<<std::endl;
+    //std::cout<<s.shortestPalindrome("aacecaaa")<<std::endl;
+    //std::cout<<s.shortestPalindrome("abcd")<<std::endl;
+
+    std::cout<<s.shortestPalindrome("aba")<<std::endl;
 
     return 0;
 }
