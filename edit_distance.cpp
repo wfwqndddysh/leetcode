9ade17cd5d286f98afc85d7fc83c3392a3ef334f
zhangjianjun <zhangfanwan@gmail.com>
2015-08-03 10:03:08 +0800

    典型的dp，当然可以降低存储来做这个道题，不过这不是训练的目的

---
+++ edit_distance.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<cassert>
+#include<string>
+#include<vector>
+#include<unordered_map>
+
+class SolutionDP
+{
+public:
+    int minDistance(const std::string& word1, const std::string& word2)
+    {
+        int m=word1.size();
+        int n=word2.size();
+
+        std::vector<std::vector<int>> dp(m+1, std::vector<int>(n+1, 0));
+
+        for(int i=1; i<=m; ++i)
+        {
+            dp[i][0]=i;
+        }
+        for(int j=1; j<=n; ++j)
+        {
+            dp[0][j]=j;
+        }
+
+
+        for(int i=1; i<=m; ++i)
+        {
+            for(int j=1; j<=n; ++j)
+            {
+                if(word1[i-1]==word2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=std::min(dp[i][j-1], std::min(dp[i-1][j], dp[i-1][j-1]))+1;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+
+int main()
+{
+    SolutionDP s;
+
+    std::string word1{"a"};
+    std::string word2{"b"};
+
+    std::cout<<s.minDistance(word1, word2)<<std::endl;
+    return 0;
+}
+
