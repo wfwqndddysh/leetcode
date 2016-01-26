e7ca811c0543e53fdd6b94b4e80382b0d0d02ec8
zhangjianjun <zhangfanwan@gmail.com>
2016-01-26 21:58:29 +0800

    this is rec, this is clean code!

--- sorted_array_to_binary_search_tree.cpp
+++ sorted_array_to_binary_search_tree.cpp
@@ -78,6 +78,60 @@ private:
     }
 };
 
+class SolutionMyOnce
+{
+public:
+    TreeNode* sortedArrayToBST(std::vector<int>& nums)
+    {
+        return rec(nums, 0, nums.size());
+    }
+private:
+    TreeNode* rec(const std::vector<int>& nums, int beg, int end)
+    {
+        if(beg==end)
+        {
+            return nullptr;
+        }
+        else if(beg+1==end)
+        {
+            return new TreeNode(nums[beg]);
+        }
+        else if(beg+2==end)
+        {
+            auto root = new TreeNode(nums[beg]);
+            root->right = new TreeNode(nums[beg+1]);
+            return root;
+        }
+        else
+        {
+            auto root = new TreeNode(nums[beg+(end-beg)/2]);
+            root->left = rec(nums, beg, beg+(end-beg)/2);
+            root->right = rec(nums, beg+(end-beg)/2+1, end);
+            return root;
+        }
+    }
+};
+
+class SolutionCleanCode
+{
+public:
+    TreeNode* sortedArrayToBST(std::vector<int>& nums)
+    {
+        return rec(nums, 0, nums.size());
+    }
+private:
+    TreeNode* rec(const std::vector<int>& nums, int beg, int end)
+    {
+        if(beg>=end) return nullptr;
+
+        int mid = beg+(end-beg)/2;
+        auto node = new TreeNode(nums[mid]);
+        node->left = rec(nums, beg, mid);
+        node->right = rec(nums, mid+1, end);
+        return node;
+    }
+};
+
 int main()
 {
     Solution s;
